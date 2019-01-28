9f4ae5dbf1beb2b099eb7b59464a737b25d72b50
Shankar Govindarajan <shgovind@redhat.com>
2019-01-28 06:35:34 -0600

    Cleaned Up

--- server.js
+++ server.js
@@ -15,12 +15,11 @@ app.listen(8080, function() {
     console.log('listening on 8080')
   })
 
-  app.get('/status', function (req, res) {
+  app.get('/', function (req, res) {
     res.send('Frontend App Status [Running]')
   })
 
   app.get('/new', function (req, res) {
-    //res.sendFile(__dirname + '/main.html')
     request('http://msg-processor:8080/payments', function(err, response, body){
       if (err) return console.log(err)
       console.log('Respons Is -->' + response.statusCode)
@@ -28,28 +27,6 @@ app.listen(8080, function() {
       res.render('main.ejs', {payments: JSON.parse(body)})
     });
   })
-
-  app.get('/', function (req, res) {
-    //res.send('Hello World')
-    request('http://msg-processor:8080/messages', function(err, response, body){
-        if (err) return console.log(err)
-        console.log('Respons Is -->' + response.statusCode)
-        console.log(body)
-        res.render('index.ejs', {messages: JSON.parse(body)})
-    });
-    //res.sendFile(__dirname + '/index.html')
-  })
-
-  app.post('/sendmsg', (req, res) => {
-    console.log(req.body.message)
-    request('http://msg-processor:8080/produce?msg='+ req.body.message, function(err, body){
-        if (err) return console.log(err)
-        console.log('Message Published Successfully')
-        //console.log(body)
-        res.redirect('/')
-    });
-  })
-
 
   app.post('/postPayment', (req, res) => {
     console.log(req.body)
